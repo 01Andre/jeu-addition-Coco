33c5394793bb0cdfa9b9398c9ff6d917915913a8
André <46403858+01Andre@users.noreply.github.com>
2020-02-09 12:15:07 +0100

    Update additions.php

--- additions.php
+++ additions.php
@@ -1,4 +1,5 @@
 <?php
+
 welcome();
 $configuration = setConfiguration();
 $password = $configuration['password'];
@@ -22,13 +23,17 @@ function setConfiguration():array
 {
     $gift = readline('Choisir la récompense. exemple : "un bonbon" ; puis appuyer sur entrée : ');
     $password = readline('Saisir le mot de passe (un caractère = une opération à réaliser) : ');
+    while (strlen($password) < 1)
+    {
+        $password = readline('Le mot de passe doit comporter au moins un caractère. Saisissez un mot de passe : ');
+    }
     $operations = addQuestions(strlen($password));
     return ['gift'=> $gift,'password'=>$password, 'operations'=> $operations];
 }
 
 function addNumber($numberOrder)
 {
-    $number = readline("Saisissez le $numberOrder nombre à additionner : ");
+    $number = getNumber("Saisissez le $numberOrder nombre à additionner : ");
     return $number;    
 }
 
@@ -37,8 +42,12 @@ function addQuestions(int $numberOfQuestions):array
     $generateAutomaticsOperations = readline("pour générer automatiquement les opérations, tapez 'y' et validez. Pour les rentrer manuellement, tapez 'n' : ");
 
     if ($generateAutomaticsOperations === 'y'){
-        $lowestInterval = readline('saisissez le nombre le plus bas auquel l\'enfant pourra être confronté : ');
-        $highestInterval = readline('saisissez le nombre le plus haut auquel l\'enfant pourra être confronté : ');
+        $lowestInterval = getNumber('saisissez le nombre le plus bas auquel l\'enfant pourra être confronté : ');
+        $highestInterval = getNumber('saisissez le nombre le plus haut auquel l\'enfant pourra être confronté : ');
+        while ($highestInterval<$lowestInterval)
+        {
+            $highestInterval = getNumber("$highestInterval est inférieur à $lowestInterval : saisissez de nouveau le nombre le plus haut auquel l'enfant pourra être confronté : ");
+        }
         return generateAutomaticallyOperations($numberOfQuestions, $lowestInterval, $highestInterval);
     }
     $questions = [];
@@ -48,6 +57,18 @@ function addQuestions(int $numberOfQuestions):array
         $questions[$i]['second number']= addNumber('second');
     }
     return $questions;
+}
+
+function getNumber(string $askingSentence) : float
+{
+    $number="";
+    while (!preg_match('/^[0-9]{0,}[.|,]{0,1}[0-9]{1,}$/',$number))
+    {
+        $number = readline($askingSentence);
+    }
+    $number = str_replace(',','.',$number);
+    settype($number,'float');
+    return $number;
 }
 
 function generateAutomaticallyOperations(int $numberOfQuestions, int $lowestInterval, int $highestInterval):array
@@ -60,7 +81,7 @@ function generateAutomaticallyOperations(int $numberOfQuestions, int $lowestInte
     return $questions;
 }
 
-function askQuestion ($number, $secondNumber, $letter, $word)
+function askQuestion (float $number, float $secondNumber, string $letter, string $word) : string
 {
     $result = $number + $secondNumber;
     $resultProposed = displayAddition($number, $secondNumber);
@@ -74,12 +95,12 @@ function askQuestion ($number, $secondNumber, $letter, $word)
     return $word;
 }
 
-function displayAddition($number, $secondNumber)
+function displayAddition(float $number, float $secondNumber):string
 {
     return readline("\n" . $number .' + ' . $secondNumber . ' = ');
 }
 
-function cleanScreen()
+function cleanScreen() : void
 {
     for ($i = 0 ; $i<100; $i++)
     {
@@ -87,12 +108,12 @@ function cleanScreen()
     }
 }
 
-function win($word)
+function win($word):void
 {
     echo "\n Bravo tu as gagné ! le mot de passe est : $word \n";
 }
 
-function startGame($gift)
+function startGame(string $gift):void
 {
     echo "Attention ! Le jeu va commencer ! \n Tu dois trouver un mot. \n 
     Ecris la solution de chaque addition, et appuie sur la touche Entrée.\n 
@@ -100,7 +121,7 @@ function startGame($gift)
     Quand tu as le mot de passe, viens me le dire, et tu gagne $gift ! \n C'est parti ! \n";
 }
 
-function putPasswordInOperationsArray($password, $operations)
+function putPasswordInOperationsArray(string $password, array $operations) : array
 {    
     $passwordArray = str_split($password);
  
@@ -111,7 +132,7 @@ function putPasswordInOperationsArray($password, $operations)
     return $operations;
 }
 
-function launchOperations($operations)
+function launchOperations(array $operations) : string
 {
     $word = '';
     foreach ($operations as $key => $operation){
@@ -123,7 +144,7 @@ function launchOperations($operations)
     return $word;
 }
 
-function play($operations)
+function play(array $operations) : void
 {
     $word = launchOperations($operations);
     win($word);
